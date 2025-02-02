a64a51cfb2b6e19eb0b0b2e951f5d3aae6f73943
Emmanuel Chibuzor <imanuelchibuzor@gmail.com>
2025-02-02 15:45:25 +0100

    create sign-in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
-import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -8,6 +10,28 @@ export const signUp = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(401, "User not found!"));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong credentials!"));
+    }
+    const token = jwt.sign({ userId: validUser._id }, process.env.JWT_SECRET);
+    const {password: pass, ...user} = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(user);
   } catch (error) {
     next(error);
   }
